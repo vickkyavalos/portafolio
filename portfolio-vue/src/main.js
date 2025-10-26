3207543e60274d67974b871842c7377b13da1443
Victoria Ines <inessavalos14@gmail.com>
2025-10-26 00:15:41 -0300

    proyecto inicial: vuejs, boostrap y gsap

---
+++ portfolio-vue/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css"; 
+
+createApp(App).mount('#app')
